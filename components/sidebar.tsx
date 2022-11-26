fa065a6803f4913a7c043578a3c30309827b042b
Mark Bozsoki <mark.bozsoki@stobag.com>
2022-11-26 21:53:13 +0100

    Change sidebar transition delay to duration

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -16,7 +16,7 @@ export const Sidebar = ({ children }: SidebarProps) => {
     <div
       className={`
       h-full flex flex-col py-6 px-4 bg-white rounded-lg
-      transition-[width] ease-in-out delay-150
+      transition-[width] ease-in-out duration-150
       ${opened ? "w-[4.5rem]" : "w-60"}`}
     >
       <div className="overflow-hidden mb-6 text-xl">{header}</div>
