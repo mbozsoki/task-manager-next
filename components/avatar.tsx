dbf79ccf8bbb3b28584fb61f605adf1e7b0e249f
Mark Bozsoki <klm09073@GV2Q314JNJ.local>
2023-03-24 22:02:26 +0100

    Use next/Image instead of img tag in Avatar component

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,20 +1,23 @@
+import Image from "next/image";
 import { FC } from "react";
 
 type AvatarProps = {
   src: string;
   onClick?: () => void;
-  alt?: string;
+  alt: string;
 };
 
 export const Avatar: FC<AvatarProps> = ({ src, alt, onClick }) => {
   return (
-    <img
+    <Image
       className={`w-12 h-12 rounded-full border-2 border-white object-cover hover:z-10 ${
         onClick && "cursor-pointer"
       }`}
       src={src}
       onClick={onClick}
       alt={alt}
+      width={48}
+      height={48}
     />
   );
 };
