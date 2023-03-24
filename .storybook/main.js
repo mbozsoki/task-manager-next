d959ad5635cf2c2bbb47cf0eaef9fe0dec22a697
Mark Bozsoki <klm09073@GV2Q314JNJ.local>
2023-03-24 21:53:59 +0100

    Upgrade to NextJS 13 and Storybook 7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,21 @@
 module.exports = {
-  stories: [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    {
-      name: "@storybook/addon-postcss",
-      options: {
-        cssLoaderOptions: {
-          importLoaders: 1,
-        },
-        postcssLoaderOptions: {
-          implementation: require("postcss"),
-        },
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", {
+    name: "@storybook/addon-postcss",
+    options: {
+      cssLoaderOptions: {
+        importLoaders: 1
       },
-    },
-  ],
-  framework: "@storybook/react",
-  core: {
-    builder: "@storybook/builder-webpack5",
+      postcssLoaderOptions: {
+        implementation: require("postcss")
+      }
+    }
+  }],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {}
   },
+  docs: {
+    autodocs: true
+  }
 };
